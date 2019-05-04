e7b07c87b0092c0db7328cc9d4251f24c218d1e4
vell_2x <vell690@yahoo.com>
2019-05-04 11:55:42 -0400

    inital commit

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string cont = "Y";
+
+            Console.WriteLine("Welcome to the Factorial Calculator!");
+
+            while(!cont.Equals("N") && !cont.Equals("n"))
+            {
+                Console.WriteLine("Please enter number you would like to find factorial for: ");
+                int num = Convert.ToInt32(Console.ReadLine());
+
+                try
+                {
+                    if (num < 0 || num > 10)
+                    {
+                        Console.WriteLine("Invalid input please try again!");
+                        continue;
+                    }
+                    Console.WriteLine("The factorial of " + num + " is " + factorial(num));
+
+                    Console.WriteLine("Would you like to continue?y/n: ");
+                    cont = Console.ReadLine();
+                }
+                catch(Exception)
+                {
+                    Console.WriteLine("Error! Please try again!");
+                    continue;
+                }
+            }
+            Console.WriteLine("Goodbye!");
+        }
+
+        public static int factorial(int num)
+        {
+            if(num == 1)
+            {
+                return 1;
+            }
+            return num * factorial(num-1);
+        }
+    }
+}
